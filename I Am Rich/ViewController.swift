9569f76855bc8f8a644c88e3dd2e54bf5e5e295a
Unknown <p.jae21@hotmail.com>
2018-02-04 18:48:38 +0000

    Added 'I Am Rich' project to the repository

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Pedro Eusébio - ISEP on 03/02/18.
+//  Copyright © 2018 Pedro Eusébio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
